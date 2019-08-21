e20e6fcceaad34420a37bf1ae6c3e835a5350efe
Wlad Kowalew <wladickowalew@gmail.com>
2019-08-21 12:42:05 +0300

    7.2.4 complete

--- Example2/app.vue
+++ Example2/app.vue
@@ -1,5 +1,16 @@
 <template>
 	<div>
-		Hello, world!
+		<p><input type="text" v-model="message"></p>
+		<par-cap v-bind:p-text="message"></par-cap>
 	</div>
 </template>
+
+<script>
+	let ParCap = require("./par-cap.vue");
+	module.exports = {
+		components: {parCap: ParCap},
+		data: function () {
+				return {message: "Hello, component2!"};
+			}
+	};
+</script>
---
+++ Example2/header.vue
@@ -0,0 +1,19 @@
+<style scoped>
+	h1{
+		color: blue;
+	}
+</style>
+
+<template>
+	<h1>{{message}}</h1>
+</template>
+
+<script>
+	module.exports = {
+		data: function() {
+			return{
+				message: "Hello, component!"
+			}
+		}
+	};
+</script>
